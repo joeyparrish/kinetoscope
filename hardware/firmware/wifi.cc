23b7a2eeffddf5f34ddc6269c172031f8005c94b
Joey Parrish <joey.parrish@gmail.com>
2024-05-03 13:23:40 -0700

    Reuse HTTP connections
    This does not seem to make a big difference in throughput

--- hardware/firmware/wifi.cc
+++ hardware/firmware/wifi.cc
@@ -13,25 +13,61 @@
 #include <ArduinoHttpClient.h>
 #include <HardwareSerial.h>
 #include <WiFi.h>
+#include <WiFiClient.h>
 
-#define PLAIN_HTTP
-#ifdef PLAIN_HTTP
-# include <WiFiClient.h>
-# define DEFAULT_PORT 80
-#else
-# include <WiFiClientSecure.h>
-# define DEFAULT_PORT 443
-#endif
+#define DEFAULT_PORT 80
 
 #include "wifi.h"
 
 static int status = WL_IDLE_STATUS;
 
-#ifdef PLAIN_HTTP
 static WiFiClient wifi_client;
-#else
-static WiFiClientSecure wifi_client;
+
+static char* current_server = NULL;
+static int current_port = 0;
+static HttpClient* https_client = NULL;
+
+// We will use persistent connections as much as possible to speed up requests.
+static bool need_new_https_client(const char* server, int port) {
+  if (!https_client) {
+    return true;
+  }
+
+  if (strcmp(server, current_server)) {
+    return true;
+  }
+
+  if (port != current_port) {
+    return true;
+  }
+
+  return false;
+}
+
+static void create_https_client(const char* server, int port) {
+  if (!need_new_https_client(server, port)) {
+#ifdef DEBUG
+    Serial.print("Reusing HTTP client to connect to ");
+    Serial.println(server);
 #endif
+    return;
+  }
+
+  if (https_client) {
+    delete https_client;
+    free(current_server);
+  }
+
+#ifdef DEBUG
+  Serial.print("Creating new HTTP client to connect to ");
+  Serial.println(server);
+#endif
+
+  https_client = new HttpClient(wifi_client, server, port);
+  https_client->connectionKeepAlive();
+  current_server = strdup(server);
+  current_port = port;
+}
 
 void wifi_init(const char* ssid, const char* password) {
   Serial.print("Attempting to connect to SSID: ");
@@ -40,9 +76,7 @@ void wifi_init(const char* ssid, const char* password) {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
   }
-  Serial.println("");
   Serial.println("Connected to WiFi!");
 
   IPAddress ip = WiFi.localIP();
@@ -53,30 +87,23 @@ void wifi_init(const char* ssid, const char* password) {
   Serial.print("Signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
-
-#ifndef PLAIN_HTTP
-  // Don't verify TLS certificates.
-  wifi_client.setInsecure();
-#endif
 }
 
 int wifi_https_fetch(const char* server, uint16_t port, const char* path,
                      int start_byte, uint8_t* data, int size) {
-  Serial.println("Starting connection to server...");
-
   if (!port) {
     port = DEFAULT_PORT;
   }
-  HttpClient https_client(wifi_client, server, port);
+  create_https_client(server, port);
 
   // Tell the library that we will send some custom headers, and it should wait
   // for us to finish those before completing the request.
-  https_client.beginRequest();
+  https_client->beginRequest();
 
   // Send a basic GET request.
-  if (https_client.get(path) != 0) {
+  if (https_client->get(path) != 0) {
     Serial.println("Failed to connect!");
-    https_client.stop();
+    https_client->stop();
     return -1;
   }
 
@@ -84,25 +111,25 @@ int wifi_https_fetch(const char* server, uint16_t port, const char* path,
   char rangeValue[128];
   snprintf(rangeValue, sizeof(rangeValue), "bytes=%d-%d",
            start_byte, start_byte + size - 1);
-  https_client.sendHeader("Range", rangeValue);
+  https_client->sendHeader("Range", rangeValue);
 
   // Tell the library that we're done sending headers.
-  https_client.endRequest();
+  https_client->endRequest();
 
   // Check the HTTP status code.
-  int status_code = https_client.responseStatusCode();
+  int status_code = https_client->responseStatusCode();
 
   // Since we sent a Range header, "200 OK" means the server ignored it.
   if (status_code == 200) {
     Serial.println("Failed!  Range request not supported?");
-    https_client.stop();
+    https_client->stop();
     return -1;
   }
 
   // FIXME: Test HTTP redirects
   if (status_code / 100 == 3) {
     Serial.println("Failed!  No redirect support!");
-    https_client.stop();
+    https_client->stop();
     return -1;
   }
 
@@ -111,21 +138,18 @@ int wifi_https_fetch(const char* server, uint16_t port, const char* path,
     Serial.print("Failed with status code ");
     Serial.print(status_code);
     Serial.println("!");
-    https_client.stop();
+    https_client->stop();
     return -1;
   }
 
   // Skip the response headers.
-  if (https_client.skipResponseHeaders() != 0) {
+  if (https_client->skipResponseHeaders() != 0) {
     Serial.println("Failed to read headers!");
-    https_client.stop();
+    https_client->stop();
     return -1;
   }
 
-  int body_length = https_client.contentLength();
-  Serial.print("Content length is ");
-  Serial.println(body_length);
-
+  int body_length = https_client->contentLength();
   if (body_length < size) {
     size = body_length;
   }
@@ -133,7 +157,7 @@ int wifi_https_fetch(const char* server, uint16_t port, const char* path,
   int bytes_read_total = 0;
   int bytes_left = size;
   while (bytes_left) {
-    int bytes_read = https_client.read(data, bytes_left);
+    int bytes_read = https_client->read(data, bytes_left);
     if (bytes_read <= 0) {
       // FIXME: Why does this return -1 if there isn't data available _yet_?
       delay(1);
@@ -151,10 +175,10 @@ int wifi_https_fetch(const char* server, uint16_t port, const char* path,
     Serial.print(" bytes instead of ");
     Serial.print(size);
     Serial.println("!");
-    https_client.stop();
+    https_client->stop();
     return -1;
   }
 
-  https_client.stop();
+  https_client->stop();
   return bytes_read_total;
 }
