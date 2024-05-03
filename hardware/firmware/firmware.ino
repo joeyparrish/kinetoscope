9031b783ab68b95431dfc5a3aad35e6d95d82e13
Joey Parrish <joey.parrish@gmail.com>
2024-05-03 13:23:59 -0700

    Clean up firmware speed test methodology, try tasks

--- hardware/firmware/firmware.ino
+++ hardware/firmware/firmware.ino
@@ -21,29 +21,18 @@
 #define SERVER "storage.googleapis.com"
 #define PATH   "/sega-kinetoscope/canned-videos/NEVER_GONNA_GIVE_YOU_UP.segavideo"
 
-static void test_sram_speed() {
-  // 450966 words = ~3s worth of audio+video data
-  // with headers and worst-case padding.
-  int data_size = 450966;
+// ~3s worth of audio+video data with headers and worst-case padding.
+#define ABOUT_3S_VIDEO_AUDIO_BYTES 901932
 
-  uint16_t* data = (uint16_t*)ps_malloc(data_size * sizeof(uint16_t));
-  if (!data) {
-    Serial.println("Failed to allocate is PSRAM!\n");
-    return;
-  }
-
+static long test_sram_speed(uint16_t* data, int data_size) {
   // Takes about 1200ms total, or about 2660ns per word.
   long start = millis();
   sram_write(data, data_size);
   long end = millis();
-
-  free(data);
-
-  Serial.print(end - start);
-  Serial.println(" ms total to write 3s of data to SRAM\n");
+  return end - start;
 }
 
-static void test_register_speed() {
+static long test_sync_token_read_speed() {
   long start = millis();
   int count = 0;
   // Takes about 470us each.
@@ -51,64 +40,49 @@ static void test_register_speed() {
     count += is_sync_token_set();
   }
   long end = millis();
+  return end - start;
+}
 
-  Serial.print(end - start);
-  Serial.println(" ms total for 1000 token reads\n");
-
-  start = millis();
+static long test_sync_token_clear_speed() {
+  long start = millis();
   // Takes about 700us each.
   for (int i = 0; i < 1000; ++i) {
     clear_sync_token();
   }
-  end = millis();
+  long end = millis();
+  return end - start;
+}
 
-  Serial.print(end - start);
-  Serial.println(" ms total for 1000 token clears\n");
-
-  start = millis();
+static long test_register_read_speed() {
+  int count = 0;
+  long start = millis();
   // Takes about 820us each.
   for (int i = 0; i < 1000; ++i) {
     count += read_register(i & 3);
   }
-  end = millis();
-
-  Serial.print(end - start);
-  Serial.println(" ms total for 1000 register reads\n");
+  long end = millis();
+  return end - start;
 }
 
-void test_wifi_speed(int first_byte) {
-  int data_size = 1024 * 1024;
-  uint8_t* data = (uint8_t*)ps_malloc(data_size);
-  if (!data) {
-    Serial.println("Failed to allocate is PSRAM!\n");
-    return;
-  }
-  memset(data, 0, data_size);
-
+static long test_wifi_speed(uint8_t* data, int data_size, int first_byte) {
   // Takes about 2.4s to fetch 3s worth of data.  FIXME: too slow!
   long start = millis();
   int bytes_read = wifi_https_fetch(SERVER, /* default port */ 0, PATH,
                                     first_byte, data, data_size);
-
   long end = millis();
-
-  Serial.print(end - start);
-  Serial.print(" ms to fetch ");
-  Serial.print(bytes_read);
-  Serial.print(" bytes over HTTPS from position ");
-  Serial.print(first_byte);
-  Serial.println(".");
-
-  Serial.print("First byte character code is ");
-  Serial.print((int)data[0]);
-  Serial.println(".\n");
-
-  free(data);
+  return end - start;
 }
+
+uint8_t* wifi_buffer;
+uint16_t* sram_buffer;
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial) {}  // Wait for serial port to connect
+  while (!Serial) { delay(10); }  // Wait for serial port to connect
+
+  // Delay startup so we can have the serial monitor attached.
+  delay(1000);
+  Serial.println("\n");
 
   psramInit();
 
@@ -119,19 +93,95 @@ void setup() {
   wifi_init(SECRET_WIFI_SSID, SECRET_WIFI_PASS);
 
   pinMode(LED_BUILTIN, OUTPUT);
+
+  wifi_buffer = (uint8_t*)ps_malloc(ABOUT_3S_VIDEO_AUDIO_BYTES);
+  if (!wifi_buffer) {
+    Serial.println("Failed to allocate WiFi buffer in PSRAM!\n");
+    while (true) { delay(1000); }
+  }
+
+  sram_buffer = (uint16_t*)ps_malloc(ABOUT_3S_VIDEO_AUDIO_BYTES);
+  if (!sram_buffer) {
+    Serial.println("Failed to allocate SRAM buffer in PSRAM!\n");
+    while (true) { delay(1000); }
+  }
+
+  Serial.println("\n");
+}
+
+volatile int tasks_done = 0;
+volatile long task0_ms = 0;
+volatile long task1_ms = 0;
+
+void task_core0(void* params) {
+  task0_ms = test_wifi_speed(wifi_buffer, ABOUT_3S_VIDEO_AUDIO_BYTES,
+                             /* first_byte= */ 0);
+  tasks_done++;
+  vTaskDelete(NULL);
+}
+
+void task_core1(void* params) {
+  task1_ms = test_sram_speed(sram_buffer,
+                             ABOUT_3S_VIDEO_AUDIO_BYTES / 2); // bytes => words
+  tasks_done++;
+  vTaskDelete(NULL);
 }
 
 void loop() {
+  long ms;
+
+  ms = test_sync_token_read_speed();
+  Serial.print(ms);
+  Serial.println(" us avg per sync token read.");  // 1000x reads, ms => us
+
+  ms = test_sync_token_clear_speed();
+  Serial.print(ms);
+  Serial.println(" us avg per sync token clear.");  // 1000x reads, ms => us
+
+  ms = test_register_read_speed();
+  Serial.print(ms);
+  Serial.println(" us avg per register read.");  // 1000x reads, ms => us
+
+  ms = test_wifi_speed(wifi_buffer, ABOUT_3S_VIDEO_AUDIO_BYTES,
+                       /* first_byte= */ 0);
+  Serial.print(ms);
+  Serial.println(" ms to fetch 3s of data over HTTP (no multitasking).");
+
+  ms = test_sram_speed(sram_buffer,
+                       ABOUT_3S_VIDEO_AUDIO_BYTES / 2); // bytes => words
+  Serial.print(ms);
+  Serial.println(" ms to write 3s of data to SRAM (no multitasking).");
+
+  tasks_done = 0;
+
+  TaskHandle_t task0;
+  TaskHandle_t task1;
+
+  xTaskCreatePinnedToCore(
+      task_core0, "task_core0",
+      10000, NULL, 1,
+      &task0, 0);
+
+  xTaskCreatePinnedToCore(
+      task_core1, "task_core1",
+      10000, NULL, 1,
+      &task1, 1);
+
+  long start = millis();
+  while (tasks_done != 2) {
+    delay(100);
+  }
+  long end = millis();
+
+  Serial.print(task0_ms);
+  Serial.println(" ms to fetch 3s of data over HTTP (multitasking).");
+
+  Serial.print(task1_ms);
+  Serial.println(" ms to write 3s of data to SRAM (multitasking).");
+
+  Serial.print(end - start);
+  Serial.println(" ms overall for both tasks in parallel.");
+
+  Serial.println("");
   delay(1000);
-  Serial.println("\n");
-
-  test_sram_speed();
-
-  test_register_speed();
-
-  test_wifi_speed(0);
-
-  test_wifi_speed(1);
-
-  while (true) { delay(1000); }
 }
