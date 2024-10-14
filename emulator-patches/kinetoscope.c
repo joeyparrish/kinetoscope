49e96f59046feb89fa4a27292fe8feaadd13a7fa
Joey Parrish <joey.parrish@gmail.com>
2024-10-13 19:14:19 -0700

    Refactor emulator global state

--- emulator-patches/kinetoscope.c
+++ emulator-patches/kinetoscope.c
@@ -67,58 +67,100 @@
 #define SIMULATED_PROCESSING_DELAY 100  // milliseconds
 
 // SRAM regions.
-#define REGION_SIZE        0x100000  // 1MB size
-#define SRAM_SIZE          (REGION_SIZE * 2)  // Both banks
+#define SRAM_BANK_0_OFFSET 0
+#define SRAM_BANK_1_OFFSET (1 << 20)  // 1MB
+#define SRAM_SIZE          (2 << 20)  // 2MB
 
-// Offset of paddingBytes field from the end of the headers
-#define PADDING_BYTES_OFFSET_FROM_END_OF_HEADER \
-  (sizeof(SegaVideoChunkHeader) - offsetof(SegaVideoChunkHeader, paddingBytes))
-
-
-static uint8_t* global_sram_buffer = NULL;
-static uint32_t global_sram_size = 0;
-static uint16_t global_command = 0;
-static uint16_t global_token = TOKEN_CONTROL_TO_SEGA;
-static uint16_t global_error = 0;
-static char* global_error_str = NULL;
-static uint16_t global_arg;
-static uint64_t global_ready_time = (uint64_t)-1;
-static char* global_video_url = NULL;
-static uint32_t global_chunk_size = 0;
-static uint32_t global_chunk_num = 0;
-static uint32_t global_chunks_left = 0;
-
-static pthread_t global_fetch_thread;
-static volatile bool global_fetch_busy = false;
-
-static bool global_compressed = false;
-static SegaVideoIndex global_index;
-
-// Where we read from next:
-static uint32_t global_video_url_start_byte;
-// Where we write to next:
-static uint32_t global_sram_offset;
-
-
-static void write_sram(uint32_t offset, const uint8_t* data, uint32_t size);
+static void write_sram(const uint8_t* data, uint32_t size);
+static void reset_sram(int bank);
 
 // Macros to complete sram_march_test in sram-common.h
-#define SRAM_MARCH_TEST_START(bank) uint32_t bank_offset = bank ? SRAM_BANK_SIZE_BYTES : 0;
-#define SRAM_MARCH_TEST_DATA(offset, data) write_sram(offset + bank_offset, &data, 1)
+#define SRAM_MARCH_TEST_START(bank) reset_sram(bank)
+#define SRAM_MARCH_TEST_DATA(offset, data) write_sram(&data, 1)
 #define SRAM_MARCH_TEST_END() {}
 
 // Defines sram_march_test()
-#include "../common/sram-common.h"
+#include "kinetoscope/common/sram-common.h"
 
 // Macros for rle-common.h
-#define SRAM_WRITE(buffer, size) { \
-  write_sram(global_sram_offset, buffer, size); \
-  global_sram_offset += size; \
-}
+#define SRAM_WRITE(buffer, size) write_sram(buffer, size)
 
 // Defines rle_to_sram()
-#include "../common/rle-common.h"
+#include "kinetoscope/common/rle-common.h"
 
+typedef struct kinetoscope_emulation_context_t {
+  // SRAM
+  // =========
+  // backing store for emulated SRAM banks
+  uint8_t sram_buffer[SRAM_SIZE];
+  // position we write to next
+  uint32_t sram_offset;
+
+  // Communication
+  // =============
+  // command port from Sega to uC
+  uint16_t command;
+  // argument port (related to command) from Sega to uC
+  uint16_t arg;
+  // control token, 1 bit, set by Sega, cleared by uC
+  uint16_t token;
+  // error token, 1 bit, set by uC, cleared by Sega
+  uint16_t error;
+  // a stored error string to deliver later when requested
+  char* error_str;
+  // time when the current command will be complete, to emulate a processing delay
+  uint64_t ready_time;
+
+  // Streaming
+  // =========
+  // URL of the current video
+  char* video_url;
+  // consistent size of uncompressed video chunks
+  uint32_t chunk_size;
+  // the number of the next chunk to fetch
+  uint32_t chunk_num;
+  // how many chunks are left
+  uint32_t chunks_left;
+  // postition we read from next
+  uint32_t video_url_start_byte;
+  // whether the content is compressed or not
+  bool compressed;
+  // the index of chunk offsets for compressed video
+  SegaVideoIndex index;
+
+  // Threading
+  // =========
+  // thread handle for fetching content
+  pthread_t fetch_thread;
+  // whether the thread is busy doing something right now
+  volatile bool fetch_busy;
+} kinetoscope_emulation_context_t;
+
+static kinetoscope_emulation_context_t kinetoscope;
+
+static void* fetch_thread(void* ignored_arg);
+
+void* kinetoscope_init() {
+#if !defined(__EMSCRIPTEN__)
+  curl_global_init(CURL_GLOBAL_ALL);
+#endif
+
+  kinetoscope.token = TOKEN_CONTROL_TO_SEGA;
+  kinetoscope.error = 0;
+  kinetoscope.error_str = NULL;
+  kinetoscope.ready_time = (uint64_t)-1;
+  kinetoscope.video_url = NULL;
+  kinetoscope.fetch_busy = false;
+  pthread_create(&kinetoscope.fetch_thread, NULL, fetch_thread, NULL);
+
+#if 0
+  // To test error handling, simulate no connection
+  report_error("Wired connection failed and WiFi not configured!  Have you tried connecting the thing to the other thing?");
+#endif
+
+  // Give the emulator the address of the SRAM buffer.
+  return kinetoscope.sram_buffer;
+}
 
 // Current time in milliseconds.
 static uint64_t ms_now() {
@@ -138,11 +180,15 @@ static uint64_t ms_now() {
 #endif
 }
 
-static void write_sram(uint32_t offset, const uint8_t* data, uint32_t size) {
-  if (offset + size > global_sram_size) {
+static void reset_sram(int bank) {
+  kinetoscope.sram_offset = bank ? SRAM_BANK_1_OFFSET : SRAM_BANK_0_OFFSET;
+}
+
+static void write_sram(const uint8_t* data, uint32_t size) {
+  if (kinetoscope.sram_offset + size > SRAM_SIZE) {
     fprintf(stderr, "Kinetoscope: tried to overflow SRAM!"
             " (offset: 0x%08x, size: 0x%08x)\n",
-            offset, size);
+            kinetoscope.sram_offset, size);
     return;
   }
 
@@ -151,7 +197,8 @@ static void write_sram(uint32_t offset, const uint8_t* data, uint32_t size) {
     // TODO: WHY?  This shouldn't be necessary!  Is this a bug in the emulator?
     // An assumption about what kind of data is in a direct-access buffer?
     // Should this nonsense only happen for little-endian builds?
-    global_sram_buffer[(offset + i) ^ 1] = data[i];
+    kinetoscope.sram_buffer[(kinetoscope.sram_offset + i) ^ 1] = data[i];
+    kinetoscope.sram_offset++;
   }
 }
 
@@ -162,29 +209,30 @@ static void report_error(const char *format, ...) {
   vsnprintf(message, sizeof(message), format, args);
   va_end(args);
 
-  if (global_error == 0) {
+  if (kinetoscope.error == 0) {
     printf("Kinetoscope: Simulating error: %s\n", message);
-    global_error = 1;
-    if (global_error_str) {
-      free(global_error_str);
+    kinetoscope.error = 1;
+    if (kinetoscope.error_str) {
+      free(kinetoscope.error_str);
     }
-    global_error_str = strdup(message);
+    kinetoscope.error_str = strdup(message);
   } else {
     printf("Kinetoscope: Ignoring error: %s\n", message);
   }
 }
 
 static void write_error_to_sram() {
-  write_sram(0, (const uint8_t*)global_error_str, strlen(global_error_str));
+  reset_sram(0);
+  write_sram((const uint8_t*)kinetoscope.error_str, strlen(kinetoscope.error_str));
 }
 
 // Writes HTTP data to SRAM.
 static size_t http_data_to_sram(char* data, size_t size, size_t n, void* ctx) {
-  if (global_compressed) {
+  if (kinetoscope.compressed) {
     rle_to_sram((const uint8_t*)data, size * n);
   } else {
-    write_sram(global_sram_offset, (const uint8_t*)data, size * n);
-    global_sram_offset += size * n;
+    write_sram((const uint8_t*)data, size * n);
+    kinetoscope.sram_offset += size * n;
   }
   return size * n;
 }
@@ -276,8 +324,10 @@ static bool fetch_range(const char* url, size_t first_byte, size_t size,
 #endif
 }
 
-static bool fetch_range_to_sram(const char* url, size_t first_byte,
-                                size_t size) {
+static bool fetch_range_to_sram(const char* url, bool compressed,
+                                size_t first_byte, size_t size) {
+  kinetoscope.compressed = compressed;
+
   // This shouldn't be necessary, but in case of an incomplete compressed
   // buffer being processed before this, reset the RLE decoder now.
   rle_reset();
@@ -285,8 +335,8 @@ static bool fetch_range_to_sram(const char* url, size_t first_byte,
   return fetch_range(url, first_byte, size, http_data_to_sram, NULL);
 }
 
-static bool fetch_to_sram(const char* url) {
-  return fetch_range_to_sram(url, 0, -1);
+static bool fetch_to_sram(const char* url, bool compressed) {
+  return fetch_range_to_sram(url, compressed, 0, -1);
 }
 
 static bool fetch_range_to_buffer(const char* url, void* data,
@@ -308,29 +358,29 @@ static void stop_video() {
 
 static void* fetch_thread(void* ignored_arg) {
   while (true) {
-    while (!global_fetch_busy) {
+    while (!kinetoscope.fetch_busy) {
       usleep(10 * 1000);  // 10ms
     }
 
-    size_t size = global_chunk_size;
-    if (global_compressed) {
-      size = global_index.chunk_offset[global_chunk_num + 1] -
-             global_video_url_start_byte;
+    size_t size = kinetoscope.chunk_size;
+    if (kinetoscope.compressed) {
+      size = kinetoscope.index.chunk_offset[kinetoscope.chunk_num + 1] -
+             kinetoscope.video_url_start_byte;
     }
 
-    if (!fetch_range_to_sram(global_video_url, global_video_url_start_byte,
-                             size)) {
+    if (!fetch_range_to_sram(kinetoscope.video_url, kinetoscope.compressed,
+                             kinetoscope.video_url_start_byte, size)) {
       char buf[64];
-      snprintf(buf, 64, "Failed to fetch video! (chunk %d)", global_chunk_num);
+      snprintf(buf, 64, "Failed to fetch video! (chunk %d)", kinetoscope.chunk_num);
       report_error(buf);
     } else {
-      global_chunk_num++;
-      global_chunks_left--;
-      global_video_url_start_byte += size;
-      global_sram_offset = (global_chunk_num & 1) ? REGION_SIZE : 0;
+      kinetoscope.chunk_num++;
+      kinetoscope.chunks_left--;
+      kinetoscope.video_url_start_byte += size;
+      reset_sram(kinetoscope.chunk_num & 1);
     }
 
-    global_fetch_busy = false;
+    kinetoscope.fetch_busy = false;
   }
 
   return NULL;
@@ -338,24 +388,24 @@ static void* fetch_thread(void* ignored_arg) {
 
 static void fetch_chunk() {
   // Check for underflow
-  if (global_fetch_busy) {
+  if (kinetoscope.fetch_busy) {
     report_error("Underflow detected! Internet too slow?");
     return;
   }
 
   // Flag the thread to start the next chunk.
-  global_fetch_busy = true;
+  kinetoscope.fetch_busy = true;
 }
 
 static void wait_for_chunk() {
-  while (global_fetch_busy) {
+  while (kinetoscope.fetch_busy) {
     usleep(10 * 1000);  // 10ms
   }
 }
 
 static void start_video() {
   // Look up the video URL.
-  uint16_t video_index = global_arg;
+  uint16_t video_index = kinetoscope.arg;
   if (video_index > 127) {
     char buf[64];
     snprintf(buf, 64, "Invalid video index requested! (%d)", (int)video_index);
@@ -385,54 +435,54 @@ static void start_video() {
   }
 
   size_t base_len = strlen(VIDEO_SERVER_BASE_URL);
-  if (global_video_url) {
-    free(global_video_url);
+  if (kinetoscope.video_url) {
+    free(kinetoscope.video_url);
   }
-  global_video_url = (char*)malloc(base_len + path_len + 1);
-  memcpy(global_video_url, VIDEO_SERVER_BASE_URL, base_len);
-  memcpy(global_video_url + base_len, path, path_len);
-  global_video_url[base_len + path_len] = '\0';
+  kinetoscope.video_url = (char*)malloc(base_len + path_len + 1);
+  memcpy(kinetoscope.video_url, VIDEO_SERVER_BASE_URL, base_len);
+  memcpy(kinetoscope.video_url + base_len, path, path_len);
+  kinetoscope.video_url[base_len + path_len] = '\0';
 
-  if (!fetch_to_buffer(global_video_url, &header, sizeof(header))) {
+  if (!fetch_to_buffer(kinetoscope.video_url, &header, sizeof(header))) {
     report_error("Failed to fetch header!");
     return;
   }
 
-  global_compressed = header.compression != 0;
+  kinetoscope.compressed = header.compression != 0;
   header.compression = 0;
 
-  if (global_compressed) {
-    if (!fetch_range_to_buffer(global_video_url, &global_index,
+  if (kinetoscope.compressed) {
+    if (!fetch_range_to_buffer(kinetoscope.video_url, &kinetoscope.index,
                                /* offset= */ sizeof(header),
-                               /* size= */ sizeof(global_index))) {
+                               /* size= */ sizeof(kinetoscope.index))) {
       report_error("Failed to fetch index!");
       return;
     }
 
     // Pre-byteswap the index.
-    for (int i = 0; i < sizeof(global_index) / 4; ++i) {
-      global_index.chunk_offset[i] = ntohl(global_index.chunk_offset[i]);
+    for (int i = 0; i < sizeof(kinetoscope.index) / 4; ++i) {
+      kinetoscope.index.chunk_offset[i] = ntohl(kinetoscope.index.chunk_offset[i]);
     }
   }
 
-  global_chunk_size = ntohl(header.chunkSize);
-  global_chunks_left = ntohl(header.totalChunks);
-  global_chunk_num = 0;
+  kinetoscope.chunk_size = ntohl(header.chunkSize);
+  kinetoscope.chunks_left = ntohl(header.totalChunks);
+  kinetoscope.chunk_num = 0;
 
   // Transfer the header.
-  global_sram_offset = 0;
-  write_sram(0, (const uint8_t*)&header, sizeof(header));
-  global_sram_offset += sizeof(header);
-  global_video_url_start_byte = sizeof(header);
-  if (global_compressed) {
-    global_video_url_start_byte = global_index.chunk_offset[0];
+  reset_sram(0);
+  write_sram((const uint8_t*)&header, sizeof(header));
+  kinetoscope.sram_offset += sizeof(header);
+  kinetoscope.video_url_start_byte = sizeof(header);
+  if (kinetoscope.compressed) {
+    kinetoscope.video_url_start_byte = kinetoscope.index.chunk_offset[0];
   }
 
   // Fill the first region.
   fetch_chunk();
   wait_for_chunk();
 
-  if (global_chunks_left) {
+  if (kinetoscope.chunks_left) {
     // Fill the second region as well.
     fetch_chunk();
     wait_for_chunk();
@@ -440,7 +490,7 @@ static void start_video() {
 }
 
 static void flip_region() {
-  if (!global_chunks_left) {
+  if (!kinetoscope.chunks_left) {
     return;
   }
 
@@ -450,86 +500,66 @@ static void flip_region() {
 static void get_video_list() {
   printf("Kinetoscope: list\n");
 
-  global_compressed = 0;
-  global_sram_offset = 0;
-  if (!fetch_to_sram(VIDEO_SERVER_CATALOG_URL)) {
+  reset_sram(0);
+  if (!fetch_to_sram(VIDEO_SERVER_CATALOG_URL, /* compressed= */ false)) {
     report_error("Failed to download video catalog!");
     return;
   }
 }
 
 static void execute_command() {
-  if (global_command == CMD_ECHO) {
+  if (kinetoscope.command == CMD_ECHO) {
     // Used by the ROM to check for the necessary streaming hardware.
-    uint16_t value = global_arg;
+    uint16_t value = kinetoscope.arg;
     printf("Kinetoscope: CMD_ECHO 0x%04x\n", value);
-    write_sram(0, (const uint8_t*)&value, sizeof(value));
-  } else if (global_command == CMD_LIST_VIDEOS) {
+    reset_sram(0);
+    write_sram((const uint8_t*)&value, sizeof(value));
+  } else if (kinetoscope.command == CMD_LIST_VIDEOS) {
     printf("Kinetoscope: CMD_LIST_VIDEOS\n");
     get_video_list();
-  } else if (global_command == CMD_START_VIDEO) {
+  } else if (kinetoscope.command == CMD_START_VIDEO) {
     printf("Kinetoscope: CMD_START_VIDEO\n");
     start_video();
-  } else if (global_command == CMD_STOP_VIDEO) {
+  } else if (kinetoscope.command == CMD_STOP_VIDEO) {
     printf("Kinetoscope: CMD_STOP_VIDEO\n");
     stop_video();
-  } else if (global_command == CMD_FLIP_REGION) {
+  } else if (kinetoscope.command == CMD_FLIP_REGION) {
     printf("Kinetoscope: CMD_FLIP_REGION\n");
     flip_region();
-  } else if (global_command == CMD_GET_ERROR) {
+  } else if (kinetoscope.command == CMD_GET_ERROR) {
     printf("Kinetoscope: CMD_GET_ERROR\n");
     write_error_to_sram();
-  } else if (global_command == CMD_CONNECT_NET) {
+  } else if (kinetoscope.command == CMD_CONNECT_NET) {
     printf("Kinetoscope: CMD_CONNECT_NET\n");
     // Nothing to do: already connected.
-  } else if (global_command == CMD_MARCH_TEST) {
+  } else if (kinetoscope.command == CMD_MARCH_TEST) {
     printf("Kinetoscope: CMD_MARCH_TEST\n");
-    sram_march_test(global_arg);
+    sram_march_test(kinetoscope.arg);
   } else {
-    report_error("Unrecognized command 0x%02X!", global_command);
+    report_error("Unrecognized command 0x%02X!", kinetoscope.command);
   }
 
   // Completed.
   printf("Kinetoscope: command complete.\n");
-  global_token = TOKEN_CONTROL_TO_SEGA;
-}
-
-void* kinetoscope_init() {
-  // TODO: Make this static!
-  global_sram_size = 2 << 20;  // 2MB
-  global_sram_buffer = malloc(global_sram_size);
-
-#if !defined(__EMSCRIPTEN__)
-  curl_global_init(CURL_GLOBAL_ALL);
-#endif
-
-  global_fetch_busy = false;
-  pthread_create(&global_fetch_thread, NULL, fetch_thread, NULL);
-
-#if 0
-  // To test error handling, simulate no connection
-  report_error("Wired connection failed and WiFi not configured!  Have you tried connecting the thing to the other thing?");
-#endif
-
-  return global_sram_buffer;
+  kinetoscope.token = TOKEN_CONTROL_TO_SEGA;
 }
 
 void *kinetoscope_write_16(uint32_t address, void *context, uint16_t value) {
   if (address == KINETOSCOPE_PORT_COMMAND) {
-    global_command = value;
+    kinetoscope.command = value;
   } else if (address == KINETOSCOPE_PORT_ARG) {
-    global_arg = value;
+    kinetoscope.arg = value;
   } else if (address == KINETOSCOPE_PORT_TOKEN) {
     // This bit is always set on write by Sega, no matter the value.
-    global_token = TOKEN_CONTROL_TO_STREAMER;
-    printf("Kinetoscope: Received command 0x%02x\n", global_command);
+    kinetoscope.token = TOKEN_CONTROL_TO_STREAMER;
+    printf("Kinetoscope: Received command 0x%02x\n", kinetoscope.command);
     // Decide when the command will execute, simulating async operation of
     // the cart's secondary processor.
-    global_ready_time = ms_now() + SIMULATED_PROCESSING_DELAY;
+    kinetoscope.ready_time = ms_now() + SIMULATED_PROCESSING_DELAY;
   } else if (address == KINETOSCOPE_PORT_ERROR) {
     printf("Kinetoscope: Clearing error bit\n");
     // This bit is always cleared on write by Sega, no matter the value.
-    global_error = 0;  // always cleared on write by Sega
+    kinetoscope.error = 0;  // always cleared on write by Sega
   } else {
     printf("Kinetoscope: Unknown address 0x%02x\n", address);
   }
@@ -549,19 +579,19 @@ void *kinetoscope_write_8(uint32_t address, void *context, uint8_t value) {
 }
 
 uint16_t kinetoscope_read_16(uint32_t address, void *context) {
-  if (ms_now() >= global_ready_time &&
-      global_token == TOKEN_CONTROL_TO_STREAMER) {
+  if (ms_now() >= kinetoscope.ready_time &&
+      kinetoscope.token == TOKEN_CONTROL_TO_STREAMER) {
     execute_command();
   }
 
   if (address == KINETOSCOPE_PORT_TOKEN) {
     // These are always 1-bit values.
-    //printf("Kinetoscope: Reading token bit: %d\n", global_token);
-    return global_token ? 1 : 0;
+    //printf("Kinetoscope: Reading token bit: %d\n", kinetoscope.token);
+    return kinetoscope.token ? 1 : 0;
   } else if (address == KINETOSCOPE_PORT_ERROR) {
-    //printf("Kinetoscope: Reading error bit: %d\n", global_error);
+    //printf("Kinetoscope: Reading error bit: %d\n", kinetoscope.error);
     // These are always 1-bit values.
-    return global_error ? 1 : 0;
+    return kinetoscope.error ? 1 : 0;
   } else {
     // Register values are write-only from the Sega.
     return 0;
